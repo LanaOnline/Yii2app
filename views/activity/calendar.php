a62770ae50494ca5462fae5f1285f0e8aa5f5d22
Svetlana Morgun <mcvetik@gmail.com>
2019-02-24 20:54:02 -0600

    create activity button in calendar

--- views/activity/calendar.php
+++ views/activity/calendar.php
@@ -5,7 +5,7 @@
  * Date: 2/24/2019
  * Time: 7:41 PM
  */
-
+use yii\helpers\Html;
 //add calendar view, pull data from db
 ?>
 
@@ -14,5 +14,6 @@
     <div class="col-md-6">
         <h1>Календарь</h1>
         <p>Списки активностей по дням</p>
+        <?= Html::a('Создать новую активность', ['/activity/create'], ['class' => 'btn btn-primary']); ?>
     </div>
 </div>
