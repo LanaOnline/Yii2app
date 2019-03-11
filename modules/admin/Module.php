d7eaf34de8a52f7b26c510d7cab21b7b34cdb1bd
Svetlana Morgun <mcvetik@gmail.com>
2019-03-10 21:18:57 -0500

    Module console app

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace app\modules\admin;
+//use yii\base\Application;
+//use yii\base\BootstrapInterface;
 
 /**
  * admin module definition class
  */
-class Module extends \yii\base\Module
+class Module extends \yii\base\Module //implements BootstrapInterface
 {
     /**
      * {@inheritdoc}
@@ -21,4 +23,16 @@ class Module extends \yii\base\Module
 
         // custom initialization code goes here
     }
+//    /**
+//     * Bootstrap method to be called during application bootstrap stage.
+//     * Change namespace with Bootstrap to use module console app
+//      (this class must implement BootstrapInterface, add ModuleName to 'bootstrap' config in console.php)
+//     * @param Application $app the application currently running
+//     */
+//    public function bootstrap($app)
+//    {
+//        if($app instanceof \yii\console\Application){
+//            $this->controllerNamespace='app\modules\ModuleName\commands';
+//        }
+//    }
 }
